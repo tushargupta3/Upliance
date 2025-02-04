86d3bc6b4677db3c22c1975a37b4215e8c3faaf5
tushargupta3 <tushar1905050400036@gmail.com>
2025-02-04 17:57:40 +0530

    Udpate

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import styles from './App.module.css'
+import Counter from './Counter/Counter'
+import Background from './Background/Background'
+import UserForm from './UserForm/UserForm';
+import TextEditor from './TextEditor/TextEditor';
+// eslint-disable-next-line no-unused-vars
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, Setcount] = useState(() => {
+    const storedCount = localStorage.getItem('count');
+    return storedCount ? parseInt(storedCount) : 0;
+  });
+  
+  const [userList,setUserList] = useState([]);
+  const [currentUser,setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem('count', count);
+  }, [count]);
+
+  useEffect(() => {
+    localStorage.setItem('userList',JSON.stringify(userList));
+  }, [userList]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <ToastContainer />
+      <div className={styles.container}>
+        <Counter count={count} change={Setcount} />
+        <TextEditor curruser={currentUser} />
+        <UserForm users={userList} saveuser={setUserList} curruser={setCurrentUser}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Background level={count} />
     </>
   )
 }
-
-export default App
---
+++ my-app/src/Counter/Counter.jsx
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect } from "react";
+import styles from './Counter.module.css';
+// eslint-disable-next-line react/prop-types
+export default function Counter({ count, change }) {
+
+    function decrement() {
+        if (count > 0) {
+            change(prev => prev - 1);
+        }
+    }
+
+    function reset() {
+        change(0);
+    }
+
+    function increment() {
+        if (count < 20) {
+            change(prev => prev + 1);
+        }
+    }
+
+    return (
+        <div className={styles.counter}>
+            <span className={styles.count}>
+                <h1>
+                    {count}
+                    <br />
+                    Counter
+                </h1>
+                <p>limit 0 - 20</p>
+            </span>
+            <div className={styles.btn_section}>
+                <button className={styles.decrease} onClick={decrement}>-</button>
+                <button className={styles.reset} onClick={reset}>Reset</button>
+                <button className={styles.increase} onClick={increment}>+</button>
+            </div>
+        </div>
+    )
+}
